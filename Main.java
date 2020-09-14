90eb0a5addb53a5d41448b48d961f9a0781616a9
Thomas Quigley <quigt7936@wrdsb.ca>
2020-09-14 15:08:56 +0000

    Created a calculator that figures out slope

--- Main.java
+++ Main.java
@@ -1,15 +1,32 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Figures out the slope given the x and y coordinates of two points
+ * @ThomasQuigley 
  */
 public class Main {
 
-  /**
-   * The method that is executed when you hit the run button.
-   * @param args the command line arguments
-   */
+
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+
+    // ask for the coordinates
+    System.out.println("Please enter in the x coordinate of the first point");
+    double x1 = input.nextInt();
+
+    System.out.println("Please enter in the y coordinate of the first point");
+    double y1 = input.nextInt();
+
+    System.out.println("Please enter in the x coordinate of the second point");
+    double x2 = input.nextInt();
+
+    System.out.println("Please enter in the y coordinate of the second point");
+    double y2 = input.nextInt();
+
+    // figure out the slope
+    double slope = (y2 - y1) / (x2 - x1);
+
+    // tell them their slope
+    System.out.println("The slope of your line would be " + slope + ".");
     
   }
 }
